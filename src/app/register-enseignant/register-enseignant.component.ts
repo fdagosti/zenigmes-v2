6a346e7022aa138ee6c217e056355fe7ed565df3
francois dagostini <francois.dagostini@gmail.com>
2022-01-22 19:10:33 -0500

    adding additional informations of users

--- src/app/register-enseignant/register-enseignant.component.ts
+++ src/app/register-enseignant/register-enseignant.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FirebaseError } from 'firebase/app';
+import { AuthService } from '../shared/services/auth.service';
 import data from './etablissements.json';
+
+
+function passwordMatchValidator(g: FormGroup) {
+  return g.get('password')?.value === g.get('confirmPassword')?.value
+    ? null : { 'mismatch': true };
+}
+
 @Component({
   selector: 'app-register-enseignant',
   templateUrl: './register-enseignant.component.html',
@@ -8,62 +17,78 @@ import data from './etablissements.json';
 })
 export class RegisterEnseignantComponent implements OnInit {
 
- ETABLISSEMENTS: any = data;
+  error: any = null;
+
+  ETABLISSEMENTS: any = data;
 
   formDetails = this.fb.group(
     {
-      nom: "",
-      prenom: "",
-      username: "",
+      nom: [""],
+      prenom: [""],
+      username: [""],
       email: ["", Validators.email],
       passwordGroup: this.fb.group({
-        password: "",
-        confirmPassword: ""
-      }),
-      country: "",
-      town: "",
-      etablissement: ""
+        password: ["", [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ["", Validators.required],
+      }, { validator: passwordMatchValidator } as AbstractControlOptions),
+      country: [""],
+      town: [""],
+      etablissement: [""],
     })
 
-  constructor(private fb: FormBuilder) { }
+
+
+  constructor(private fb: FormBuilder, private auth: AuthService) { }
 
   ngOnInit(): void {
   }
 
   onSubmit(): void {
-    console.log("submitting form ",this.formDetails)
+    console.log("submitting form ", this.formDetails)
+    const email = this.formDetails.get("email")?.value;
+    const password = this.formDetails.get("passwordGroup")?.get("password")?.value;
+    const username = this.formDetails.get("username")?.value;
+    const role = "enseignant"
+    const pays = this.formDetails.get("country")?.value;
+    const ville = this.formDetails.get("town")?.value;
+    const etablissement = this.formDetails.get("etablissement")?.value;
+
+    this.auth.register(email, password, username, role, pays, ville, etablissement).then(
+      success => { },
+      (error: any) => this.error = error)
   }
 
-  get email(){
+
+  get email() {
     return this.formDetails.get("email");
   }
 
-  getCountries(){
+  get pwdGroup() {
+    return this.formDetails.get("passwordGroup");
+  }
+
+  getCountries() {
     return Object.keys(this.ETABLISSEMENTS);
   }
 
-  getVilles(country: any){
-    console.log("get ville ", country)
+  getVilles(country: any) {
     if (!country) return [];
     return Object.keys(this.ETABLISSEMENTS[country]);
 
   }
 
-  getEtablissements(country:any, ville:any){
-    console.log("get etablissement ", country, ville)
+  getEtablissements(country: any, ville: any) {
     if (!country || !ville) return [];
     return this.ETABLISSEMENTS[country][ville];
   }
 
-  getSelectedCountry(): any{
+  getSelectedCountry(): any {
     let result = this.formDetails.get("country")?.value
-    console.log("slected country ", result)
     return result
   }
 
-  getSelectedVille(): any{
+  getSelectedVille(): any {
     let result = this.formDetails.get("town")?.value
-    console.log("slected ville ", result)
     return result
   }
 
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,31 +1,13 @@
 import { Injectable } from '@angular/core';
-
 import {
-  Auth,
-  signOut,
-  signInWithPopup,
-  user,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  updateProfile,
-  sendEmailVerification,
-  sendPasswordResetEmail,
-  getAdditionalUserInfo,
-  OAuthProvider,
-  linkWithPopup,
-  unlink,
-  updateEmail,
-  updatePassword,
-  User,
-  reauthenticateWithPopup,
-  authState,
-  onAuthStateChanged
+  Auth, createUserWithEmailAndPassword, sendPasswordResetEmail, signInWithEmailAndPassword, user, User, UserCredential
 } from '@angular/fire/auth';
-
-
-import { Firestore } from '@angular/fire/firestore';
+import { collection, Firestore, getDocs } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
+import { doc, query, setDoc, where } from 'firebase/firestore';
 import { lastValueFrom, Observable, take } from 'rxjs';
+
+
 
 
 
@@ -40,39 +22,63 @@ export class AuthService {
   constructor(
     public afs: Firestore,   // Inject Firestore service
     public auth: Auth, // Inject Firebase auth service
-    public router: Router,  
-  ) { 
+    public router: Router,
+  ) {
 
     this.user$ = user(auth);
-  
+
   }
 
   async getUser(): Promise<User | null> {
     return await lastValueFrom(this.user$.pipe(take(1)));
   }
-  
-  async emailLogin(email: string, password: string)
-  : Promise<any> {
+
+  async login(email: string, password: string)
+    : Promise<any> {
     return await signInWithEmailAndPassword(this.auth, email, password);
   }
-  
-  async emailSignUp(email: string, password: string)
-  : Promise<void> {
-  
+
+  async register(email: string, password: string, username: string, role: string, pays: string, ville: string, etablissement: string)
+    : Promise<void> {
+
+
+    const existingUsername = await this.usernameAlreadyExist(username);
+    if (existingUsername) {
+      return Promise.reject("Le nom d'utilisateur existe déja")
+    }
+
     const credential = await createUserWithEmailAndPassword(
       this.auth,
       email,
       password
     );
-    await updateProfile(
-      credential.user, { displayName: credential.user.displayName }
-    );
-    await sendEmailVerification(credential.user);
-  
+
+    this.updateProfile(credential, username, role, pays, ville, etablissement);
+
+
   }
-  
+
+  async usernameAlreadyExist(username: string) {
+    const profilesRef = collection(this.afs, "profiles")
+    const q = query(profilesRef, where("username", "==", username));
+    const querySnapshot = await getDocs(q);
+    console.log("username exists ? ", (querySnapshot.size > 0))
+    return querySnapshot.size > 0;
+  }
+
+  async updateProfile(credential: UserCredential, username: string, role: string, pays: string, ville: string, etablissement: string) {
+    await setDoc(doc(this.afs, "profiles", credential.user.uid), {
+      username,
+      pays,
+      ville,
+      etablissement,
+      role 
+    }); 
+
+  }
+
   async resetPassword(email: string): Promise<any> {
-  
+
     // sends reset password email
     await sendPasswordResetEmail(this.auth, email);
   }
